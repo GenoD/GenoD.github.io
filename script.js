c698caf924c4b251a4348a8d0ce67f389377ac78
Geno <genodiaz@yahoo.com>
2016-07-20 21:13:41 -0700

    Formatted Input boxes
    Cleaned up the input boxes and changed overall font

--- script.js
+++ script.js
@@ -1,24 +1,54 @@
-$(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if ($(".navbar").offset().top > 50) {
+            console.log($('askdlf'));
+            $(".navbar-fixed-top").addClass("top-nav-collapse");
+        } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        }
+    });
 
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+    // $('.nav a').on('click', function() {
+    //     $('.navbar-toggle').click() //bootstrap 3.x by Richard
+    // });
 
-      // Store hash
-      var hash = this.hash;
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+
+    // function checkOffset() {
+    //     if ($(".navbar").offset().top > 50) {
+    //         $(".navbar-fixed-top").addClass("top-nav-collapse");
+    //     } else {
+    //         $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    //     }
+    // }
+
+    // // Run it when the page loads
+    // checkOffset();
+
+
+    // // Run function when scrolling
+    // $(window).scroll(function() {
+    //     checkOffset();
+    // });
 });
